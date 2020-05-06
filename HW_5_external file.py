fbeb2d2acd2040ab8dba7ea5610f7ca24e14febb
Valeria <v910423@gmail.com>
2020-05-06 17:31:58 +0300

    HW5

--- HW_5_external file.py
+++ HW_5_external file.py
@@ -1,14 +1,29 @@
 source = "/Users/valeriabelko/Desktop/Python/HW 5.txt"
 with open(source) as file:
     text = file.read().split('\n')
+    print(text)
 
-    spisok = [int(item) for item in text]
+    int_spisok = []
+    for st in text:
+        try:
+            int_spisok.append(int(st))
+        except ValueError:
+            continue
 
+    print(int_spisok)
+    a = sum(int_spisok) / len(int_spisok)
+    print(round(a,2))
 
-    summa = "Сумма чисел равна " + str(sum(spisok))
-    avrg = "Среднее значение равно " + str(sum(spisok)/len(spisok))
+#     spisok = [int(item) for item in text]
+
+    summa = "Сумма чисел равна " + str(sum(int_spisok))
+    avrg = "Среднее значение равно " + str(round(sum(int_spisok)/len(int_spisok),2))
+    min = "Мин значение равно " + str(min(int_spisok))
+    maks = "Max значение равно " + str(max(int_spisok))
 
 results = '/Users/valeriabelko/Desktop/Python/Results.txt'
 with open(results, 'w') as file2:
-    file2.write(avrg + '\n' + summa)
+    file2.write(avrg + '\n' + summa + '\n' + min + '\n' + maks)
+
+
 
