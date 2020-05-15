320bf1bb0075473ddd2d33ea408f5c1b23a9f556
Valeria <v910423@gmail.com>
2020-05-15 17:43:32 +0300

    --

--- HW_7_Regular expr.py
+++ HW_7_Regular expr.py
@@ -6,16 +6,29 @@ quest = input('do u want to add user?: ').upper()
 if quest != "Y":
     exit()
 while answer == "Y":
-    login = input("Введите логин. Он должен содержать более трех знаков и начинаться с заглавной буквы, не должен содержать символы и пробелы. " )
-    # result_login = re.fullmatch(r'[A-z0-9]{3,}',login)
+    login = input("Введите логин. Он должен содержать более трех знаков и не должен содержать символы и пробелы. " )
+
+    while not re.fullmatch(r'[A-z0-9]{3,}',login):
+        login = input(('Введенный login не соответсвует формату. Повторите ввод: '))
+
     if re.fullmatch(r'[A-z0-9]{3,}',login):
-        email = input('Введите адрес электронной почты: ')
-        # result_email = re.fullmatch(r'\S*@\S*.\S',email)
-        if re.fullmatch(r'\S*@\S*.\S',email):
-            spisok[email] = login
+        if login in spisok.values():
+            print('Этот login уже занят.')
         else:
-            email = input('Введите адрес электронной почты: ')
+            email = input('Введите адрес электронной почты: ')  #v910423@gmail.com
+
+            while not re.fullmatch(r'^[\w\-_\"\.]*@[a-z]+\.[a-z]+',email):
+                email = input('Введенный адрес электронной почты не соответсвует формату. Повторите ввод почты: ')
+
+            if re.fullmatch(r'\S*@\S*.\S',email):
+                if email in spisok:
+                    print('Этот адрес электронной почты уже занят для логина.')
+                else:
+                    spisok[email] = login
+                    print('User is added.')
+
     answer = input('do u want to add user?: ').upper()
-# print(result_login)
-# print(result_email)
 print(spisok)
+
+with open("HW_7.txt", 'w') as file:
+    file.write(str(spisok))
